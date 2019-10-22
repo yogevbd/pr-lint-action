d832fe41df9fddbc7b94d005de803fb6515794a0
Itai <i.schuftan@gmail.com>
2019-10-22 13:58:51 +0300

    Moved validators to config file

--- pr-lint.config.js
+++ pr-lint.config.js
@@ -10,7 +10,28 @@ module.exports = {
       beginsWith: "#### Changelog",
       endsWith: "#### Breaking changes",
       message: "Changelog section is mandatory",
-      minimumLength: 10
-    }
-  ]
+      validate: (section) => {
+        return section.length > 10;
+      }
+    },
+    {
+      beginsWith: "#### Jira issue",
+      endsWith: "#### Changelog",
+      message: "Changelog section is mandatory",
+      validate: (section) => {
+        if (section === '') { 
+          return true
+        } else {
+          const validTicketsFormat = ['WOA', 'WOAI', 'CHAT', 'FRM', 'GROUP', 'OAV', 'CSF', 'WCN'];
+          const exists = validTicketsFormat.map(format => {
+              const re = new RegExp(format);
+              console.log(section);
+              const result = re.test(section.toUpperCase());
+              return result;
+          });
+          return exists.includes(true);
+        }
+      }
+    },
+  ],
 }
